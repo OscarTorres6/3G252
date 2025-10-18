4fb92c88273541a2ad87a953e555c346a1ad8704
OscarTorres6 <a24310308@live.ceti.mx>
2025-10-18 01:34:56 +0000

    practica 4

--- src/ejemplo.cpp
+++ src/ejemplo.cpp
@@ -1,32 +1,66 @@
-#include<iostream>
+#include <iostream>
+#include<thread>
+#include<chrono>
+#include<string>
+#include<sstream>
+
 using namespace std;
 
-#include<ftxui/screen/screen.hpp>
-#include<ftxui/screen/color.hpp>
-#include<ftxui/component/component.hpp>
-#include<ftxui/component/screen_interactive.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 
 using namespace ftxui;
 
-
+string nave =
+string ("       ___        \n")+
+string ("   ___/   \\___   \n")+
+string ("  /   '---'   \\  \n")+
+string (" ' --_______--'   \n")+
+string ("       / \\       \n")+
+string ("      /   \\      \n")+
+string ("      /\\O/\\     \n")+
+string ("      / | \\      \n")+
+string ("      // \\\\     \n");
 int main(int argc, char const *argv[])
 {
     auto pantalla = Screen::Create(
         Dimension::Full(),
-        Dimension::Fixed(10)
-    );
+        Dimension::Fixed(10));
 
     int x = 0;
     int y = 0;
+    
 
-    auto& pixel = pantalla.PixelAt(10, 10);
-    pixel.blink = true;
-    pixel.background_color = Color::Blue;
-    pixel.foreground_color = Color::Red;
-    pixel.character = 'o';
+    while (true)
+    {
+        x++;
 
-    pantalla.Print();
+        stringstream ss(nave);
+        string linea;
+        int noLinea=0;
+        while (getline(ss, linea))
+        {
+            int columna = 0;
+            for(auto &&letra : linea)
+            {
+            auto &pixel = pantalla.PixelAt(x + columna, y + noLinea);
+            pixel.character = letra;
+            columna++;
+            }
+            noLinea++;
+        }
+        //pixel.blink = true;
+        //pixel.background_color = Color::Blue;
+        //pixel.foreground_color = Color::Red;
+      
 
+        pantalla.Print();
+        pantalla.Clear();
+        this_thread::sleep_for(chrono::milliseconds(500));
+        cout << pantalla.ResetPosition();
+    }
 
     return 0;
 }
